d8f6aba327237053845c37c5d4ce89c474a90cef
MiChel Causey <michel.causey@gmail.com>
2019-04-09 20:30:14 -0400

    additional files

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div> App Component</div>
+        <div> Hi Asad! </div>
     )
 }
 
--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-require('dotenv').config({ debug: process.env.debug })
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
-const db = require('./models')
+// require('dotenv').config({ debug: process.env.debug })
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+// const db = require('./models')
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
-let syncOptions = { force: false }
+// let syncOptions = { force: false }
 
-if (process.env.NODE_ENV === 'test') {
-  syncOptions.force = true
-}
+// if (process.env.NODE_ENV === 'test') {
+//   syncOptions.force = true
+// }
 
-// Hello this is my big commit change Shrublord for president - I'll drink to Shrublord. 
+// // Hello this is my big commit change Shrublord for president - I'll drink to Shrublord. 
 
-// api routes will go here eventually
+// // api routes will go here eventually
 
-// app.listen(PORT, function() {
-//     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-//   });
-db.sequelize.sync(syncOptions).then(() => {
-  app.listen(PORT, () => {
-    console.log(`🌎 Listening on Port ${PORT}`)
-  })
-})
+// // app.listen(PORT, function() {
+// //     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+// //   });
+// db.sequelize.sync(syncOptions).then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`🌎 Listening on Port ${PORT}`)
+//   })
+// })
