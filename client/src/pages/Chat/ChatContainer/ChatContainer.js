f62ca41b84899dec3b2ed7deaf34c9c6c289f1fa
smoot <smoothtacomann@gmail.com>
2019-04-11 17:21:47 -0400

    added basic class component function boiler plate

---
+++ client/src/pages/Chat/ChatContainer/ChatContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ChatContainer extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div> ChatContainer</div>
+        )
+    }
+
+}
+
+export default ChatContainer;
--- client/src/pages/LandingPage/SignUpModal/SignUpModal.js
+++ client/src/pages/LandingPage/SignUpModal/SignUpModal.js
@@ -1,1 +1,17 @@
 // Modal
+import React from 'react';
+
+class SignUpModal extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div> SignUpModal</div>
+        )
+    }
+
+}
+
+export default SignUpModal;
