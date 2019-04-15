bf0da77cb66f9a9a33ac654a7f0afb54d8bb56e0
smoot <smoothtacomann@gmail.com>
2019-04-15 19:35:19 -0400

    chat page container and its containers updated

--- client/src/pages/Chat/ChatContainer/ChatContainer.js
+++ client/src/pages/Chat/ChatContainer/ChatContainer.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React from "react";
+import "../ChatContainer/ChatContainer.css";
 
-class ChatContainer extends React.Component{
-    constructor(props){
-        super(props)
-    }
+class ChatContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return(
-            <div> ChatContainer</div>
-        )
-    }
+  render() {
+    return (
+      <div className="chat-wrapper">
+        <div className="chat-navbar-container">
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+            <li>Link 4</li>
+          </ul>
+        </div>
 
+        <div className="chat-sidebar-container">
+
+        side bar
+        
+        </div>
+
+        <div className="chat-box-container">chat box</div>
+
+        <div className="chat-footer-container">footer</div>
+      </div>
+    );
+  }
 }
 
 export default ChatContainer;
