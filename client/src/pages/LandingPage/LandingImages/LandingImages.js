254eef0ecb3e1de6e12d334bdaf386981c5c4192
MiChel Causey <michel.causey@gmail.com>
2019-05-13 19:55:37 -0400

    slideshow update

--- client/src/pages/LandingPage/LandingImages/LandingImages.js
+++ client/src/pages/LandingPage/LandingImages/LandingImages.js
@@ -1,51 +1,52 @@
 import React from 'react';
-import { Slide } from 'react-slideshow-image';
+import { Fade } from 'react-slideshow-image';
 import '../LandingImages/LandingImages.css'
  
-const properties = {
-  duration: 5000,
-  transitionDuration: 500,
-  infinite: true,
+ 
+const fadeProperties = {
+  duration: 3000,
+  transitionDuration: 800,
+  infinite: false,
   indicators: true,
   arrows: true
 }
- 
+
 const LandingImages = () => {
     return (
-      <Slide {...properties}>
-        <div className="each-slide">
+      <Fade {...fadeProperties}>
+        <div className="each-fade">
           <div className="image"><img src='/images/1.jpg'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/2.jpeg'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/3.jpg'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/4.jpg'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/5.JPG'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/6.JPG'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/7.JPG'></img>
           </div>
         </div>
-        <div className="each-slide">
+        <div className="each-fade">
           <div className="image"><img src='/images/8.jpg'></img>
           </div>
         </div>
-      </Slide>
+      </Fade>
     )
 }
 
