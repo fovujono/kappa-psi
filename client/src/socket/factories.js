48b4959848019d8f04469af8a3719d03a6f54bab
smoot <smoothtacomann@gmail.com>
2019-05-27 21:07:36 -0400

    added socket events for different scenerios the user might encounter

---
+++ client/src/socket/factories.js
@@ -0,0 +1,15 @@
+// const uuid = require("uuid")
+
+
+// const createUser = ({name} = {})=> (
+//     {
+//     id: uuid(),
+//     name})
+
+
+
+// const createMessage = ({'message':, 'sender':""} = { })=>({
+//     id: uuid(),
+//     time: new Date(Date.now())
+
+// })
